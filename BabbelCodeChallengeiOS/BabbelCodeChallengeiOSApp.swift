c121358bbfee128787dd1e51abe87340ee385825
Douglas Gandle <doug.gandle@gmail.com>
2022-11-17 07:45:58 -0500

    Initial Commit

---
+++ BabbelCodeChallengeiOS/BabbelCodeChallengeiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabbelCodeChallengeiOSApp.swift
+//  BabbelCodeChallengeiOS
+//
+//  Created by Doug Gandle on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct BabbelCodeChallengeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
